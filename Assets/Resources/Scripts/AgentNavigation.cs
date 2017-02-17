1dc7504abae056620a9d7ed9631c10481c1bea60
BrandonT202 <brandon.t202@gmail.com>
2017-02-17 13:52:15 +0000

    Initial Scripts

---
+++ Assets/Resources/Scripts/AgentNavigation.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AgentNavigation : MonoBehaviour
+{
+    // Open list for nodes currently able to visit
+    private List<Node> m_OpenList = new List<Node>();
+
+    // Closed list for nodes visited
+    private List<Node> m_ClosedList = new List<Node>();
+
+    // Starting Node
+    private GameObject m_StartNode;
+
+    // End Node
+    private GameObject m_EndNode;
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_StartNode = GameObject.FindWithTag("StartNode");
+        m_EndNode = GameObject.FindWithTag("EndNode");
+
+        m_ClosedList.Add(new Node
+        {
+            NodeId = 0,
+            EndNode = false,
+            FValue = 0,
+            HeuristicCost = 0
+        });
+	}
+	
+	void FixedUpdate ()
+    {
+        
+	}
+}
---
+++ Assets/Resources/Scripts/Node.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node
+{
+    public int NodeId { get; set; }
+    public int HeuristicCost { get; set; }
+    public int FValue { get; set; }
+    public bool EndNode { get; set; }
+}
