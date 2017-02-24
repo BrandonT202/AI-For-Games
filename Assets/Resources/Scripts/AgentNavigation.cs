3f0abeef01c2796875fa09f089cbc180dbe56fda
BrandonT202 <brandon.t202@gmail.com>
2017-02-24 16:44:34 +0000

    Algorithm finds the end (attempt 1)

--- Assets/Resources/Scripts/AgentNavigation.cs
+++ Assets/Resources/Scripts/AgentNavigation.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 
 public class AgentNavigation : MonoBehaviour
 {
-    // Open list for nodes currently able to visit
+    enum Direction { NORTH = 0, EAST, SOUTH, WEST, NUMOFDIRECTIONS };
+
+    // Open list for nodes currently able to be visited
     private List<Node> m_OpenList = new List<Node>();
 
     // Closed list for nodes visited
-    private List<Node> m_ClosedList = new List<Node>();
+    public List<Node> m_ClosedList = new List<Node>();
+    public int ListSize;
 
     // Starting Node
     private GameObject m_StartNode;
@@ -18,10 +20,11 @@ public class AgentNavigation : MonoBehaviour
 
     private Node m_CurrentNode;
 
-    public const int m_DirectionCost = 10; //Horizontal/Vertical direction
+    //private Node m_Last
 
-    private bool direction = true;
-    private bool positive = false;
+    public int m_currentHeuristicCost = 0;
+
+    public const int m_DirectionCost = 10; // Horizontal / Vertical direction
 
     public Mesh mesh;
 
@@ -33,7 +36,9 @@ public class AgentNavigation : MonoBehaviour
     private GameObject CurrentNode;
 
     public Material nodeMat;
+    public Material nodeMat2;
     public Material CurrNodeMat;
+    float timer;
 
     // Use this for initialization
     void Start()
@@ -42,11 +47,12 @@ public class AgentNavigation : MonoBehaviour
         m_StartNode = GameObject.FindWithTag("StartNode");
         m_EndNode = GameObject.FindWithTag("EndNode");
 
+        // Add start node to the closed list
         m_ClosedList.Add(new Node
         {
             NodeId = new Vector2(10, 10),
             EndNode = false,
-            FValue = 42,
+            FinalValue = 42,
             HeuristicCost = 42
         });
         m_CurrentNode = m_ClosedList[0];
@@ -63,102 +69,128 @@ public class AgentNavigation : MonoBehaviour
 
     void FixedUpdate()
     {
-        Node node = new Node();
-        node.HeuristicCost = (int)((m_CurrentNode.NodeId.x - m_EndNode.transform.position.x) + (m_CurrentNode.NodeId.y - m_EndNode.transform.position.z));
-        node.FValue = node.HeuristicCost + m_DirectionCost;
+        /* 
+         * Add start node to the closed list
+         * Find the next node
+         * Add that node to the open list
+         * Calculate heuristic cost and G cost (directional cost)
+         * 
+        */
 
-        if (direction)
+        timer += Time.deltaTime;
+        if (timer >= 0.05f)
         {
-            if (positive)
+            timer = 0;
+            for (Direction m_direction = Direction.NORTH; m_direction < Direction.NUMOFDIRECTIONS; m_direction++)
             {
-                node.NodeId = new Vector2(m_CurrentNode.NodeId.x + 1, m_CurrentNode.NodeId.y);
-            }
-            else
-            {
-                node.NodeId = new Vector2(m_CurrentNode.NodeId.x - 1, m_CurrentNode.NodeId.y);
-            }
-        }
-        else
-        {
-            if (positive)
-            {
-                node.NodeId = new Vector2(m_CurrentNode.NodeId.x, m_CurrentNode.NodeId.y + 1);
-            }
-            else
-            {
-                node.NodeId = new Vector2(m_CurrentNode.NodeId.x, m_CurrentNode.NodeId.y - 1);
-            }
-        }
-
-        m_currentPos = node.NodeId;
-        try
-        {
-            if (failCounter <= 0)
-            {
-                failCounter = maxFailCounter;
-                if (direction)
+                Node node = new Node();
+                switch (m_direction)
                 {
-                    if (positive)
+                    case Direction.NORTH:
+                        node.NodeId = new Vector2(m_CurrentNode.NodeId.x, m_CurrentNode.NodeId.y + 1);
+                        break;
+                    case Direction.EAST:
+                        node.NodeId = new Vector2(m_CurrentNode.NodeId.x + 1, m_CurrentNode.NodeId.y);
+                        break;
+                    case Direction.SOUTH:
+                        node.NodeId = new Vector2(m_CurrentNode.NodeId.x, m_CurrentNode.NodeId.y - 1);
+                        break;
+                    case Direction.WEST:
+                        node.NodeId = new Vector2(m_CurrentNode.NodeId.x - 1, m_CurrentNode.NodeId.y);
+                        break;
+                    default:
+                        break;
+                }
+
+                try
+                {
+                    if (Physics.OverlapSphere(new Vector3(node.NodeId.x, 0.5f, node.NodeId.y), 0.1f)[0])
                     {
-                        m_CurrentNode.NodeId = new Vector2(m_CurrentNode.NodeId.x - 1, m_CurrentNode.NodeId.y);
-                    }
-                    else
-                    {
-                        m_CurrentNode.NodeId = new Vector2(m_CurrentNode.NodeId.x + 1, m_CurrentNode.NodeId.y);
                     }
                 }
-                else
+                catch
                 {
-                    if (positive)
-                    {
-                        m_CurrentNode.NodeId = new Vector2(m_CurrentNode.NodeId.x, m_CurrentNode.NodeId.y - 1);
-                    }
-                    else
-                    {
-                        m_CurrentNode.NodeId = new Vector2(m_CurrentNode.NodeId.x, m_CurrentNode.NodeId.y + 1);
-                    }
+                    node.HeuristicCost = (int)((node.NodeId.x - m_EndNode.transform.position.x) + (node.NodeId.y - m_EndNode.transform.position.z));
+                    node.FinalValue = node.HeuristicCost + m_DirectionCost;
+                    m_OpenList.Add(node);
                 }
-                CurrentNode.transform.position = new Vector3(m_CurrentNode.NodeId.x, 1.5f, m_CurrentNode.NodeId.y);
-                return;
+
             }
-            if (Physics.OverlapSphere(new Vector3(node.NodeId.x, 0.5f, node.NodeId.y), 0.1f)[0].GetComponent<MeshFilter>().mesh.name == mesh.name)
+
+            Node closestNode = new Node();
+            closestNode.FinalValue = int.MaxValue;
+            closestNode.NodeId = m_ClosedList[m_ClosedList.Count - 1].NodeId;
+            foreach (Node listNode in m_OpenList)
             {
-                Debug.Log(m_currentPos);
-                m_CurrentNode = node;
-                //newNode(m_CurrentNode);
+                // Find closest to end node
+                if (closestNode.FinalValue >= listNode.FinalValue)
+                {
+                    closestNode = listNode;
+                }
             }
-            else
+
+            // Add current node to closed list
+            if (m_CurrentNode.NodeId != new Vector2(m_EndNode.transform.position.x, m_EndNode.transform.position.z))
             {
-                if(direction)
-                    positive = !positive;
-                direction = !direction;
-                //if(!direction)
+                newNode(closestNode);
+                RemoveNodeFromOpenList(closestNode);
+                if (/*m_CurrentNode.NodeId != m_ClosedList[m_ClosedList.Count - 1].NodeId*/m_OpenList.Count > 0 || m_ClosedList.Count < 2)
+                {
+                }
+                /*else//dead end
+                {
+                    Debug.Log("Dead End");
+                    GameObject tempObj =  GameObject.Find(m_ClosedList[m_ClosedList.Count - 1].NodeId.x + " : " + m_ClosedList[m_ClosedList.Count - 1].NodeId.y);
+                    tempObj.GetComponent<MeshRenderer>().material = nodeMat2;
+                    m_ClosedList.Remove(m_ClosedList[m_ClosedList.Count - 1]);
+                    closestNode = m_ClosedList[m_ClosedList.Count - 1];
+
+
+                }*/
             }
-            failCounter--;
+            else//reached end goal [ Check the open list for a valid contender for closest node ]
+                this.enabled = false;
+
+            // Make closest node current node
+            m_CurrentNode = closestNode;
+
+            m_currentHeuristicCost = m_CurrentNode.HeuristicCost;
+            m_currentPos = m_CurrentNode.NodeId;
+            ListSize = m_ClosedList.Count;
+            //m_OpenList.Clear();
         }
-        catch
+    }
+
+    void RemoveNodeFromOpenList(Node node)
+    {
+        m_ClosedList.Add(node);
+
+        //Debug.Log(m_OpenList.Count);
+        while (m_OpenList.Count > 20)
         {
-            //CurrentNode.transform.position = new Vector3(node.NodeId.x, 0.5f, node.NodeId.y) +  Vector3.Angle(transform.position, new Vector3(node.NodeId.x, 0.5f, node.NodeId.y))
-            CurrentNode.transform.position = new Vector3(node.NodeId.x, 1.5f, node.NodeId.y);
-            failCounter = maxFailCounter;
-            newNode(node);
-            direction = !direction;
+            GameObject tempObj = GameObject.Find(m_OpenList[0].NodeId.x + " : " + m_OpenList[0].NodeId.y);
+            Destroy(tempObj);
+            m_OpenList.Remove(m_OpenList[0]);//potential for shorter memmory but needs work
+
         }
 
-        foreach (Node listNode in m_OpenList)
+
+        foreach (var listNode in m_OpenList) //TODO: Identified problem with the open list - redundant node inside REMOVE
         {
-
+            if (listNode.NodeId == node.NodeId)
+            {
+                GameObject tempObj = GameObject.Find(listNode.NodeId.x + " : " + listNode.NodeId.y);
+                tempObj.GetComponent<MeshRenderer>().material = nodeMat2;
+                m_OpenList.Remove(listNode);
+                break;
+            }
         }
-
-        //m_CurrentNode = node;
-
     }
 
     void newNode(Node node)
     {
         //Add new node to open list
         Debug.Log("Found valid node");
-        m_OpenList.Add(node);
         GameObject obj = new GameObject();
         obj.transform.position = new Vector3(node.NodeId.x, 0.5f, node.NodeId.y);
         obj.name = node.NodeId.x + " : " + node.NodeId.y;
--- Assets/Resources/Scripts/Node.cs
+++ Assets/Resources/Scripts/Node.cs
@@ -5,6 +5,6 @@ public class Node
 {
     public Vector2 NodeId { get; set; }
     public int HeuristicCost { get; set; }
-    public int FValue { get; set; }
+    public int FinalValue { get; set; }
     public bool EndNode { get; set; }
 }
